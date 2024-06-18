4cede771ee2ea3c60ae2eb4d23c37fd417cce7fc
seungwonme <seungwonan.me@gmail.com>
2024-06-18 22:59:00 +0900

    feat: Create HTTPException class and Add verifyRequest method in RequestHandler class

--- src/requestHandler/RequestHandler.cpp
+++ src/requestHandler/RequestHandler.cpp
@@ -1,44 +1,76 @@
 #include "RequestHandler.hpp"
 
-void RequestHandler::handleRequest(const RequestMessage& req, ResponseMessage& res, const ServerConfig& serverConfig) {
+void RequestHandler::verifyRequestLine(const RequestLine& reqLine)
+{
+    const std::string method = reqLine.getMethod();
+    const std::string req_url = reqLine.getRequestURL();
+    const std::string ver = reqLine.getHTTPVersion();
+    if (method != "GET" && method != "POST" && method != "DELETE" && method != "PUT")
+    {
+        throw HTTPException(METHOD_NOT_ALLOWED, "Method Not Allowed");
+    }
+    if (ver != "HTTP/1.1")
+    {
+        throw HTTPException(HTTP_VERSION_NOT_SUPPORTED, "HTTP Version Not Supported");
+    }
+    if (req_url[0] != '/')
+    {
+        throw HTTPException(NOT_FOUND, "Not Found");
+    }
+}
+void RequestHandler::verifyRequestHeaderFields(
+    const HeaderFields& reqHeaderFields)
+{
+    if (reqHeaderFields.hasField("Host") == false)
+    {
+        throw HTTPException(BAD_REQUEST, "Bad Request");
+    }
+}
+void RequestHandler::verifyRequest(const RequestMessage& req, const ServerConfig& serverConfig)
+{
+    try
+    {
+        verifyRequestLine(req.getRequestLine());
+        verifyRequestHeaderFields(req.getRequestHeaderFields());
+    }
+    catch (const HTTPException& e)
+    {
+        throw e;
+    }
 
-	// std::map<std::string, LocationConfig>::const_iterator it = serverConfig.locations.find(request.getPath());
-	// if (it != serverConfig.locations.end()) {
-	// 	response.setStatusCode(404, "OK");
-	// 	response.setHeader("Content-Type", "text/plain");
-	// 	response.setBody("Not Found");
+    (void)serverConfig;
+}
+void RequestHandler::handleRequest(const RequestMessage& req, ResponseMessage& res, const ServerConfig& serverConfig)
+{
+    // std::map<std::string, LocationConfig>::const_iterator it =
+    // serverConfig.locations.find(request.getPath()); if (it !=
+    // serverConfig.locations.end()) { 	response.setStatusCode(404, "OK");
+    // 	response.setHeader("Content-Type", "text/plain");
+    // 	response.setBody("Not Found");
 
-	// 	return ;
-	// }
+    // 	return ;
+    // }
 
-	// const LocationConfig& locConfig = it->second;
+    // const LocationConfig& locConfig = it->second;
 
-	// if (std::find(locConfig.allow_methods.begin(), locConfig.allow_methods.end(), request.getMethod()) == locConfig.allow_methods.end()) {
-	// 	response.setStatusCode(405, "test");
-	// 	response.setHeader("Content-Type", "text/plain");
-	// 	response.setBody("Method Not Allowed");
+    // if (std::find(locConfig.allow_methods.begin(),
+    // locConfig.allow_methods.end(), request.getMethod()) ==
+    // locConfig.allow_methods.end()) { 	response.setStatusCode(405, "test");
+    // 	response.setHeader("Content-Type", "text/plain");
+    // 	response.setBody("Method Not Allowed");
 
-	// 	return ;
-	// }
-	(void)req;
-	(void)serverConfig;
+    // 	return ;
+    // }
+    (void)req;
+    (void)serverConfig;
 
-    res.setStatusLine("HTTP/1.1", "200", "OK");
-    res.addResponseHeaderField("Content-Type", "text/plain");
-    res.addResponseHeaderField("Content-Length", "2");
-    res.addMessageBody("OK");
-	// res.setStatusCode(200, "OK");
-	// res.setHeader("Content-Type", "text/plain");
-	// res.setBody("OK");
+    res.setStatusLine(
+        req.getRequestLine().getHTTPVersion(), std::to_string(OK), "OK");
+    res.addResponseHeaderField("Content-Type", "text/html");
+    res.addMessageBody("<html><body><h1>Request Received</h1></body></html>");
 }
 
-void RequestHandler::badRequest(ResponseMessage& res, std::string body) {
-
-    res.setStatusLine("HTTP/1.1", "400", "NO");
-    res.addResponseHeaderField("Content-Type", "text/plain");
-    res.addMessageBody("Bad Request: " + body);
-	// response.setStatusCode(400, "NO");
-	// response.setHeader("Content-Type", "text/plain");
-	// response.setBody("Bad Request: " + body);
+void RequestHandler::handleException(const HTTPException& e, ResponseMessage& res)
+{
+    res.setStatusLine("HTTP/1.1", e.getStatusCode(), e.getReasonPhrase());
 }
-
