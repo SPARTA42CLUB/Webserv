5bb491924ed0813d8fab6cd2a6753f4f91df88e1
AhnJoonSung <tjdtna01@naver.com>
2024-06-18 22:59:36 +0900

    check allowed method & request-url

--- src/requestHandler/RequestHandler.cpp
+++ src/requestHandler/RequestHandler.cpp
@@ -2,34 +2,33 @@
 
 void RequestHandler::handleRequest(const RequestMessage& req, ResponseMessage& res, const ServerConfig& serverConfig) {
 
-	// std::map<std::string, LocationConfig>::const_iterator it = serverConfig.locations.find(request.getPath());
-	// if (it != serverConfig.locations.end()) {
-	// 	response.setStatusCode(404, "OK");
-	// 	response.setHeader("Content-Type", "text/plain");
-	// 	response.setBody("Not Found");
+	std::string url = req.getRequestLine().getRequestURL();
+	std::string method = req.getRequestLine().getMethod();
 
-	// 	return ;
-	// }
+	std::map<std::string, LocationConfig>::const_iterator it = serverConfig.locations.find(url);
+	if (it == serverConfig.locations.end()) {
+		res.setStatusLine("HTTP/1.1", "404", "NO");
+		res.addResponseHeaderField("Content-Type", "text/plain");
+		res.addMessageBody("Not Found");
 
-	// const LocationConfig& locConfig = it->second;
+		return ;
+	}
 
-	// if (std::find(locConfig.allow_methods.begin(), locConfig.allow_methods.end(), request.getMethod()) == locConfig.allow_methods.end()) {
-	// 	response.setStatusCode(405, "test");
-	// 	response.setHeader("Content-Type", "text/plain");
-	// 	response.setBody("Method Not Allowed");
+	const LocationConfig& locConfig = it->second;
 
-	// 	return ;
-	// }
-	(void)req;
-	(void)serverConfig;
+	if (std::find(locConfig.allow_methods.begin(), locConfig.allow_methods.end(), method) == locConfig.allow_methods.end()) {
+		res.setStatusLine("HTTP/1.1", "405", "NO");
+		res.addResponseHeaderField("Content-Type", "text/plain");
+		res.addMessageBody("Method Not Allowed");
+
+		return ;
+	}
+
 
     res.setStatusLine("HTTP/1.1", "200", "OK");
     res.addResponseHeaderField("Content-Type", "text/plain");
-    res.addResponseHeaderField("Content-Length", "2");
     res.addMessageBody("OK");
-	// res.setStatusCode(200, "OK");
-	// res.setHeader("Content-Type", "text/plain");
-	// res.setBody("OK");
+
 }
 
 void RequestHandler::badRequest(ResponseMessage& res, std::string body) {
@@ -37,8 +36,5 @@ void RequestHandler::badRequest(ResponseMessage& res, std::string body) {
     res.setStatusLine("HTTP/1.1", "400", "NO");
     res.addResponseHeaderField("Content-Type", "text/plain");
     res.addMessageBody("Bad Request: " + body);
-	// response.setStatusCode(400, "NO");
-	// response.setHeader("Content-Type", "text/plain");
-	// response.setBody("Bad Request: " + body);
 }
 
