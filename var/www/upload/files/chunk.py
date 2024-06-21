eade44fa4f3489ba5d6157f908fdea7b7c94b61b
Seongmin Kim <seongmik@c5r8s1.42seoul.kr>
2024-06-21 16:45:24 +0900

    [Test]: 청크 요청 테스트파일 추가

---
+++ var/www/upload/files/chunk.py
@@ -0,0 +1,41 @@
+import http.client
+import time
+
+UPLOAD_HOST = "localhost"
+UPLOAD_PORT = 8081
+UPLOAD_PATH = "/upload"
+IMAGE_FILE = "testImage.png"
+CHUNK_SIZE = 1024
+
+def send_file_in_chunks(file_path, chunk_size, conn):
+    with open(file_path, "rb") as f:
+        while True:
+            chunk = f.read(chunk_size)
+            if not chunk:
+                break
+            actual_chunk_size = len(chunk)
+            padded_chunk = chunk + b'\x00' * (chunk_size - actual_chunk_size)
+            chunk_size_hex = format(len(padded_chunk), 'x').encode()
+            conn.send(b"%s\r\n%s\r\n" % (chunk_size_hex, padded_chunk))
+            time.sleep(0.05)
+
+def main():
+    conn = http.client.HTTPConnection(UPLOAD_HOST, UPLOAD_PORT)
+    
+    headers = {
+        "Transfer-Encoding": "chunked",
+        "Content-Type": "image/png"
+    }
+    
+    conn.request("POST", UPLOAD_PATH, headers=headers)
+    # Wait for 1 second before starting to send chunks
+    time.sleep(1)
+    
+    send_file_in_chunks(IMAGE_FILE, CHUNK_SIZE, conn)
+    conn.send(b"0\r\n\r\n")
+    
+    print("File sent successfully.")
+    conn.close()
+
+if __name__ == "__main__":
+    main()
