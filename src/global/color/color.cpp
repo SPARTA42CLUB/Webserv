db434af784862515682452c9571506f3ad008175
seungwonme <seungwonan.me@gmail.com>
2024-06-24 04:25:10 +0900

    feat: Add path to location config and finish parsing configs

---
+++ src/global/color/color.cpp
@@ -0,0 +1,30 @@
+#include "color.hpp"
+
+namespace color
+{
+    const char* FG_BLACK = "\033[90m";
+    const char* FG_RED = "\033[91m";
+    const char* FG_GREEN = "\033[92m";
+    const char* FG_YELLOW = "\033[93m";
+    const char* FG_BLUE = "\033[94m";
+    const char* FG_MAGENTA = "\033[95m";
+    const char* FG_CYAN = "\033[96m";
+    const char* FG_WHITE = "\033[97m";
+    const char* FG_DEFAULT = "\033[39m";
+    const char* DEFAULT = "\033[0m";
+    const char* BOLD = "\033[1m";
+    const char* ITALIC = "\033[3m";
+    const char* UNDERLINE = "\033[4m";
+    const char* REVERSE = "\033[7m";
+    const char* STRIKETHROUGH = "\033[9m";
+    const char* BG_BLACK = "\033[100m";
+    const char* BG_RED = "\033[101m";
+    const char* BG_GREEN = "\033[102m";
+    const char* BG_YELLOW = "\033[103m";
+    const char* BG_BLUE = "\033[104m";
+    const char* BG_MAGENTA = "\033[105m";
+    const char* BG_CYAN = "\033[106m";
+    const char* BG_WHITE = "\033[107m";
+    const char* BG_DEFAULT = "\033[49m";
+    const char* RESET = "\033[0m";
+}
--- src/global/config/Config.hpp
+++ src/global/config/Config.hpp
@@ -5,27 +5,28 @@
 #include <vector>
 
 const size_t DEFAULT_TIMEOUT = 60;
+const size_t MAX_TIMEOUT = 600;
 const size_t DEFAULT_MAX_BODY_SIZE = 1024;  // 1KB
-const int PORT_MAX = 65535;
+const int MAX_PORT_NUM = 65535;
 
 struct LocationConfig
 {
     std::string root;
-    std::string alias;
     std::string index;
     std::vector<std::string> allow_methods;
     bool directory_listing;
     std::string redirect;
     std::string cgi;
+    std::string path;
 
     LocationConfig()
     : root()
-    , alias()
     , index()
     , allow_methods()
     , directory_listing(false)
     , redirect()
     , cgi()
+    , path()
     {
     }
 };
@@ -34,6 +35,7 @@ struct ServerConfig
 {
     std::string host;
     int port;
+    std::string root;
     size_t client_max_body_size;
     std::map<size_t, std::string> error_pages;
     std::map<std::string, LocationConfig> locations;
@@ -41,6 +43,7 @@ struct ServerConfig
     ServerConfig()
     : host()
     , port(-1)
+    , root()
     , client_max_body_size(DEFAULT_MAX_BODY_SIZE)
     , error_pages()
     , locations()
@@ -61,24 +64,27 @@ private:
     // Parse ServerConfig
     void parseHost(ServerConfig& serverConfig, std::string& value);
     void parsePort(ServerConfig& serverConfig, std::string& value);
+    void parseRoot(ServerConfig& serverConfig, std::string& value);
     void parseClientMaxBodySize(ServerConfig& serverConfig, std::string& value);
     void parseErrorPage(ServerConfig& serverConfig, std::string& value);
     void parseLocation(std::ifstream& file, ServerConfig& serverConfig, std::string& locationPath);
     // Parse LocationConfig
     void parseRoot(LocationConfig& locationConfig, std::string& value);
-    void parseAlias(LocationConfig& locationConfig, std::string& value);
     void parseIndex(LocationConfig& locationConfig, std::string& value);
     void parseAllowMethods(LocationConfig& locationConfig, std::string& value);
     void parseDirectoryListing(LocationConfig& locationConfig, std::string& value);
     void parseRedirect(LocationConfig& locationConfig, std::string& value);
-    void parseCgi(LocationConfig& locationConfig, std::string& value);
+    void parseCGI(LocationConfig& locationConfig, std::string& value);
     // Check Validity
     bool isValidValue(std::string& value);
     bool isValidLocationPath(std::string& locationPath);
+    
+    void verifyConfig(void);
+    void makePath(const std::string& loc, LocationConfig& locConf);
 
 public:
     Config(const std::string& configFilePath);
     const std::vector<ServerConfig>& getServerConfigs() const;
     // TEST: 파싱 결과 출력
-    void print(void);
+    void print(void) const;
 };
