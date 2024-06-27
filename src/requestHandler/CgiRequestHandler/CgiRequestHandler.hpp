4dcb58dc914504ceeef99da2ca3148cc5c9d4718
Ingyu Lim <inlim@c1r8s2.42seoul.kr>
2024-06-27 20:16:51 +0900

    CgiRequestHandler

---
+++ src/requestHandler/CgiRequestHandler/CgiRequestHandler.hpp
@@ -0,0 +1,18 @@
+class CgiRequestHandler
+{
+private:
+	Connection &mConnection;
+	RequestMessage& mReq;
+	ResponseMessage mResponseMessage;
+	std::string& mUri;
+	std::string& mRequestBody;
+	std::string &mMethod;
+	std::string &mCgiInterpreter;
+	int mClientSocket;
+public:
+	CgiRequestHandler(std::string &uri,std::string &requestBody, std::string &method, std::string &cgi);
+	void excuteCgi();
+	void makeCgiResponse();
+	~CgiRequestHandler();
+};
+
